89e60f4939199b9ce06479d2e1d9c444e0a6ab8a
Achmad Syahrul Hanafi <55965861+achmadsyahrul@users.noreply.github.com>
2020-04-24 04:46:58 +0700

    Ini class kerucut
    monggo dicoret2, diedit ae nek ono sing salah

---
+++ src/app/Kerucut.java
@@ -0,0 +1,50 @@
+package app;
+
+public class Kerucut extends Lingkaran{
+    private double tinggi;
+
+    Kerucut(){
+        
+    }
+
+    Kerucut(double tinggi, double radius){
+        this.tinggi = tinggi;
+        super.setR(radius);
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    private double hitungGarisPelukis(){
+        //Garis Pelukis = s
+        //s = pitagoras radius & tinggi
+        return Math.sqrt(Math.pow(super.getRadius(),2)+Math.pow(this.tinggi,2));
+    }
+
+    public double getGarisPelukis(){
+        return hitungGarisPelukis();
+    }
+
+    private double hitungVolume(){
+        //Volume Keurcut = 1/3 * Luas alas * Tinggi
+        // 1/3 * phi * r^2 * r
+        return 1/3.0 * super.getLuas() * this.tinggi;
+    }
+
+    public double getVolume(){
+        return hitungVolume();
+    }
+
+    private double hitungLuas(){
+        return phi*super.getRadius()*(super.getRadius()+this.getGarisPelukis());
+    }
+
+    public double getLuas(){
+        return this.hitungLuas();
+    }
+}
