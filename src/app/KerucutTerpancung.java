bf286659259667048f0d92ac9ffae1f5909296d0
Achmad Syahrul Hanafi <55965861+achmadsyahrul@users.noreply.github.com>
2020-04-24 15:49:25 +0700

    Add Luas Permukaan

--- src/app/KerucutTerpancung.java
+++ src/app/KerucutTerpancung.java
@@ -62,4 +62,29 @@ public class KerucutTerpancung extends Kerucut{
         return hitungVolume();
     }
 
+    private double hitungA(){
+        //A = sqrt(t^2 + (r1-r2)^2)
+        return Math.sqrt(Math.pow(super.getTinggi(), 2) + Math.pow((Math.abs(super.getRadius()-this.radius2)),2));
+    }
+
+    private double hitungLuasSelimut(){
+        //hanya luas selimut
+        // L = phi * A (r1 + r2)
+        return phi * hitungA() * (super.getRadius()+this.radius2);
+    }
+
+    public double getLuasSelimut(){
+        return hitungLuasSelimut();
+    }
+
+    private double hitungLuas(){
+        //luas permukaan atau luas seluruhnya
+        // L = Luas selimut + luas lingkaran (besar + kecil)
+        return getLuasSelimut() + phi * (Math.pow(super.getRadius(), 2) + Math.pow(this.radius2, 2));
+    }
+
+    public double getLuas(){
+        return hitungLuas();
+    }
+
 }
